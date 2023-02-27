9aefba1ff52544ed18129b904063514c51f09674
unknown <halhhute@gmail.com>
2023-02-27 15:42:26 +0700

    Edit BT3 thuế thu nhập === pp tính Lũy tiến

--- js/method.js
+++ js/method.js
@@ -3,10 +3,10 @@
  * @param {*} so số điện người dùng nhập vào
  * @returns hàm trả về giá trị tiền điện cần đóng
  */
-function tinhTienDien(so){
-    var output=0;
+function tinhTienDien(so) {
+    var output = 0;
     if (so == '' || so <= 0) {
-        output=0;
+        output = 0;
     } else {
         if (so <= 50) {
             output = 500 * so;
@@ -14,15 +14,15 @@ function tinhTienDien(so){
         else if (so > 50 && so <= 100) {
             output = 50 * 500 + (so - 50) * 650;
         }
-        else if (so > 100 && soDien <= 200) {
+        else if (so > 100 && so <= 200) {
             output = 50 * 500 + 50 * 650 + (so - 100) * 850;
         }
-        else if (so > 200 && soDien <= 350) {
+        else if (so > 200 && so <= 350) {
             output = 50 * 500 + 50 * 650 + 100 * 850 + (so - 200) * 1100;
         }
         else if (so > 350) {
             output = 50 * 500 + 50 * 650 + 100 * 850 + 150 * 1100 + (so - 350) * 1300;
-        }               
+        }
     }
     return output;
 }
@@ -41,20 +41,26 @@ function tinhThueThuNhap(tongLuongNam, soNguoiPhuThuoc) {
     // Output: thuNhapChiuThue (number), thuePhaiTra(number)
     var thuNhapChiuThue = tongLuongNam - 4000000 - (soNguoiPhuThuoc * 1600000);
     var output = 0;
-    if (thuNhapChiuThue <= 60000000) {
+    if (thuNhapChiuThue <= (60e+6)) {
         output = thuNhapChiuThue * 0.05;
-    } else if (thuNhapChiuThue > 60000000 && thuNhapChiuThue <= 120000000) {
-        output = thuNhapChiuThue * 0.1;
-    } else if (thuNhapChiuThue > 120000000 && thuNhapChiuThue <= 210000000) {
-        output = thuNhapChiuThue * 0.15;
-    } else if (thuNhapChiuThue > 210000000 && thuNhapChiuThue <= 384000000) {
-        output = thuNhapChiuThue * 0.2;
-    } else if (thuNhapChiuThue > 384000000 && thuNhapChiuThue <= 624000000) {
-        output = thuNhapChiuThue * 0.25;
-    } else if (thuNhapChiuThue > 624000000 && thuNhapChiuThue <= 960000000) {
-        output = thuNhapChiuThue * 0.3;
-    } else if (thuNhapChiuThue > 960000000) {
-        output = thuNhapChiuThue * 0.35;
+    } else if (thuNhapChiuThue > (60e+6) && thuNhapChiuThue <= (120e+6)) {
+        // output = thuNhapChiuThue * 0.1;
+        output = (60e+6) * 0.05 + ((120e+6) - thuNhapChiuThue) * 0.1
+    } else if (thuNhapChiuThue > (120e+6) && thuNhapChiuThue <= (210e+6)) {
+        // output = thuNhapChiuThue * 0.15;
+        output = (60e+6) * 0.05 + (60e+6) * 0.1 + ((210e+6) - thuNhapChiuThue) * 0.15
+    } else if (thuNhapChiuThue > (210e+6) && thuNhapChiuThue <= (384e+6)) {
+        // output = thuNhapChiuThue * 0.2;
+        output = (60e+6) * 0.05 + (60e+6) * 0.1 + (90e+6) * 0.15 + ((384e+6) - thuNhapChiuThue) * 0.2
+    } else if (thuNhapChiuThue > (384e+6) && thuNhapChiuThue <= (624e+6)) {
+        // output = thuNhapChiuThue * 0.25;
+        output = (60e+6) * 0.05 + (60e+6) * 0.1 + (90e+6) * 0.15 + (174e+6)*0.2 + ((624e+6)-thuNhapChiuThue)*0.25
+    } else if (thuNhapChiuThue > (624e+6) && thuNhapChiuThue <= (960e+6)) {
+        // output = thuNhapChiuThue * 0.3;
+        output = (60e+6) * 0.05 + (60e+6) * 0.1 + (90e+6) * 0.15 + (174e+6)*0.2 + (240e+6)*0.25 + ((960e+6)-thuNhapChiuThue)*0.3
+    } else if (thuNhapChiuThue > (960e+6)) {
+        // output = thuNhapChiuThue * 0.35;
+        output = (60e+6) * 0.05 + (60e+6) * 0.1 + (90e+6) * 0.15 + (174e+6)*0.2 + (240e+6)*0.25 + (336e+6)*0.3 + (thuNhapChiuThue-(960e+6))*0.35;
     }
 
     return output;
